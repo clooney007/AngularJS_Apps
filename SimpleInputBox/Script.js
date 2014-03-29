023718b2044e17576be66aff2a19a9cf433311af
clooney007 <clooney_zby@hotmail.com>
2014-03-30 12:22:52 +1300

    Create gameList controller

---
+++ SimpleInputBox/Script.js
@@ -0,0 +1,9 @@
+function gameList($scope){
+    $scope.games = [
+        {name:"World of Warcraft",rate:5},
+        {name:"Diablo 3", rate:3},
+        {name:"Dota 2", rate:5},
+        {name:"The Elder Scroll 5", rate:4},
+        {name:"The Elder Scroll Online", rate:3}
+    ];
+}
